83749f98f84d247813c30a10ede5c39f5ccfd12a
Shubham Rana <s4shubham.rana20@gmail.com>
2023-11-09 20:20:46 +0530

    working on migration certificate

--- resources/views/admin/pdf/migration-certificate.blade.php
+++ resources/views/admin/pdf/migration-certificate.blade.php
@@ -9,10 +9,10 @@
 
 </head>
 <body>
-    <table style="width: 100%; border: 1px solid black;" class="mt-3">
-        <tr>
+    <table style="width: 100%;">
+        <tr style="width: 100%;" class="mt-3" style="border: 1px groove black; border-bottom: 0px;">
             <td style="width:100%; font-size: 14px;" class="px-1">
-                <table style="width: 100%;">
+                <table style="width: 100%;" class="mt-2">
                     <tr>
                         <td style="width:33%;">
                             <p class="m-0 p-0">
@@ -35,20 +35,20 @@
                                 <span class="fw-bold mb-1">
                                     Transfer Certificate File No. :
                                 </span>
-                                    {{ $student->transfer_certificate->certificate_no }}
+                                {{ $student->transfer_certificate->certificate_no }}
                             </p>
                         </td>
                     </tr>
                 </table>
             </td>
         </tr>
-        <tr class="text-center mb-1">
+        <tr class="text-center mb-1" style="border: 1px groove black;">
             <td>
                 <h1 class="fw-bold m-0 my-2">Scholar's Register & Transfer Certificate Form</h1>
             </td>
         </tr>
-        <tr class="border border-1">
-            <td class="p-1" style="font-size: 14px;">
+        <tr style="border: 1px groove black; border-bottom: 0px;">
+            <td class="p-1" style="font-size: 15px;">
                 <p class="m-0 p-0">
                     <span class="fw-bold">Name of Institution:</span>
                     H.L.S. PUBLIC SCHOOL KURSINDA GOTANI KUNDA PRAPTAPGARH U.P. 230202
@@ -57,7 +57,7 @@
         </tr>
     </table>
     <table style="width: 100%;">
-        <tr class="px-1 border border-1" style="vertical-align: top;">
+        <tr class="px-1" style="vertical-align: top; border: 1px groove black;">
             <td class="border border-1 text-center" style="width: 25%; font-size: 14px;">
                 <p class="m-0 p-0">
                     <span class="fw-bold">Scholar's Name</span>
@@ -66,7 +66,7 @@
                     {{ $student->name }}
                 </p>
             </td>
-            <td class="border border-1 text-center" style="width: 25%; font-size: 14px;">
+            <td class="text-center" style="width: 25%; font-size: 14px; border: 1px groove black;">
                 <p class="m-0 p-0">
                     <span class="fw-bold">
                         Name , occupatin and address
@@ -75,7 +75,7 @@
                     {{ $student->transfer_certificate->occupation }}
                 </p>
             </td>
-            <td class="border border-1 text-center" style="width: 25%; font-size: 14px;">
+            <td class="text-center" style="width: 25%; font-size: 14px; border: 1px groove black;">
                 <p class="m-0 p-0">
                     <span class="fw-bold">
                         Date of the birth of the scholar
@@ -84,7 +84,7 @@
                     {{ date('d-M-Y' , strtotime($student->dob)) }}
                 </p>
             </td>
-            <td class="border border-1 text-center" style="width: 25%; font-size: 14px;">
+            <td class="text-center" style="width: 25%; font-size: 14px; border: 1px groove black;">
                 <p class="m-0 p-0">
                     <span class="fw-bold">
                         Last institution attended before joining this one if any
@@ -96,17 +96,17 @@
     </table>
     <table style="width: 100%;">
         <tr class="px-1 border border-1 border-top-0" style="vertical-align: top;">
-            <td class="border border-1  border-top-0 text-center" style="width: 25%; font-size: 14px;">
+            <td class="text-center" style="width: 25%; font-size: 14px;">
                 <p class="m-0 p-0">
                     <span class="fw-bold">Cast or Religion</span>
                     <br/>
                     {{ $student->transfer_certificate->cast_or_religion }}
                 </p>
             </td>
-            <td class="border border-1 border-top-0 text-center" style="width: 25%; font-size: 14px;">
+            <td class="text-center border border-1 border-top-0" style="width: 25%; font-size: 14px;">
                 <table style="width: 100%;">
-                    <tr>
-                        <td style="width: 100%; border-bottom:1px solid black;" class="text-center pb-1">
+                    <tr style="width: 100%; border-bottom:1px groove black;">
+                        <td style="width: 100%;" class="text-center pb-1">
                             <p class="m-0 p-0">
                                 <span class="fw-bold">
                                     Mother's Name
@@ -131,9 +131,7 @@
                     </tr>
                 </table>
             </td>
-            <td class="border border-1 border-top-0 text-center" style="width: 25%; font-size: 14px;">
-                <p class="m-0 p-0">
-                </p>
+            <td class="text-center" style="width: 25%; font-size: 14px; border-left: 1.4px groove black;">
             </td>
             <td class="border border-1 border-top-0 text-center" style="width: 25%; font-size: 14px;">
                 <p class="m-0 p-0">
@@ -147,41 +145,60 @@
         </tr>
     </table>
     <table style="width: 100%;">
-        <tr class="border border-1 border-top-2">
-            <td style="width: 33%;">
-                <table style="width: 100%;">
-                    <tr style="vertical-align: middle; border-bottom: 2px solid black; font-size: 14px;">
-                        <th style="border-right: 1px solid black; text-align: center; width: 20%;">Class</th>
-                        <th style="border-right: 1px solid black; text-align: center; width: 25%;">Date of Admission</th>
-                        <th style="border-right: 1px solid black; text-align: center; width: 25%;">Date of Promotion</th>
-                        <th style="border-right: 1px solid black; text-align: center; width: 30%;">Date of Removal</th>
-                    </tr>
-                    <tr style="font-size: 12px; width:100%;">
-                        <td style="border-right: 1px solid rgb(139, 128, 128); text-align: center; width: 20%; font-size: 14px;" class="fw-bold">IV</td>
-                        <td style="border-right: 1px solid black; text-align: center; width: 25%;">{{ date('d-m-Y', strtotime($student->transfer_certificate->date_of_admission)) }}</td>
-                        <td style="border-right: 1px solid black; text-align: center; width: 25%;">{{ date('d-m-Y', strtotime($student->transfer_certificate->date_of_promotion)) }}</td>
-                        <td style="border-right: 1px solid black; text-align: center; width: 30%;">{{ date('d-m-Y', strtotime($student->transfer_certificate->date_of_removal)) }}</td>
-                    </tr>
-                </table>
-            </td>
-            <td style="width: 30%">
-                <table style="width: 100%;">
-                    <tr style="vertical-align: middle; font-size: 14px;">
-                        <th>
-                            Cause of Removal e.g non payment of dues , removal of family, explusion etc.
-                        </th>
-                    </tr>
-                    <tr>
-                        <td>
-                            {{  }}
-                        </td>
-                    </tr>
-                </table>
-            </td>
-            <td style="width: 37%">
-                <table style="width: 100%;"></table>
-            </td>
+        <tr class="border border-1" style="vertical-align: middle; font-size: 14px;">
+            <th style="border-right: 1px groove black; border-left: 1px groove black; text-align: center;" class="py-2 px-1">Class</th>
+            <th style="border-right: 1px groove black; text-align: center;" class="py-2">Date of Admission</th>
+            <th style="border-right: 1px groove black; text-align: center;" class="py-2">Date of Promotion</th>
+            <th style="border-right: 1px groove black; text-align: center;" class="py-2">Date of Removal</th>
+            <th style="border-right: 1px groove black; text-align: center;" class="py-2">
+                Cause of Removal e.g non payment of dues , removal of family, explusion etc.
+            </th>
+            <th style="border-right: 1px groove black; text-align: center;">Year or Session</th>
+            <th style="border-right: 1px groove black; text-align: center;">Conduct or Concession</th>
+            <th style="border-right: 1px groove black; text-align: center;">Work</th>
         </tr>
+        @php
+            $jsonData               = json_decode($student->transfer_certificate->class);
+            $date_of_admission      = json_decode($student->transfer_certificate->date_of_admission, true);
+            $date_of_promotion      = json_decode($student->transfer_certificate->date_of_promotion, true);
+            $date_of_removal        = json_decode($student->transfer_certificate->date_of_removal, true);
+            $cause_of_removal       = json_decode($student->transfer_certificate->cause_of_removal, true);
+            $year_or_session        = json_decode($student->transfer_certificate->year_or_session, true);
+            $conduct_or_concession  = json_decode($student->transfer_certificate->conduct_or_concession, true);
+            $work                   = json_decode($student->transfer_certificate->work, true);
+        @endphp
+        @isset($jsonData)
+        @foreach ($jsonData as $key => $item)
+            <tr style="font-size: 13px;  width:100%;" class="border border-1">
+                <td style="text-align: center; border-right: 1px groove black;" class="fw-bold" rowspan="3">{{ $item }}</td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;">{{ $date_of_admission[$key] ? date('d-m-Y', strtotime($date_of_admission[$key])): "" }}</td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;">{{ $date_of_promotion[$key] ? date('d-m-Y', strtotime($date_of_promotion[$key])) : ""}}</td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;">{{ $date_of_removal[$key] ? date('d-m-Y', strtotime($date_of_removal[$key])) : ""}}</td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;">{{ $cause_of_removal[$key] ?? "" }}</td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;">{{ $year_or_session[$key] ?? "" }}</td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;">{{ $conduct_or_concession[$key] ?? "" }}</td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;">{{ $work[$key] ?? "" }}</td>
+            </tr>
+            <tr style="font-size: 13px; width:100%;" class="border border-1">
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;"></td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;"></td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;"></td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;"></td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;"></td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;"></td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;"></td>
+            </tr>
+            <tr style="font-size:13px; width:100%;" class="border border-1">
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;"></td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;"></td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;"></td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;"></td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;"></td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;"></td>
+                <td style="text-align: center; border-right: 1px groove black; height: 20px;"></td>
+            </tr>
+        @endforeach
+        @endisset
     </table>
 </body>
 </html>
